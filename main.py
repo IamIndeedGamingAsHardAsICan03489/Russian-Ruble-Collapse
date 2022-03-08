f614d4047464c993a7f8b8fd5179ed0668fc1f35
IamIndeedGamingAsHardAsICan03489 <81194357+IamIndeedGamingAsHardAsICan03489@users.noreply.github.com>
2022-03-08 15:25:32 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from pickle import TRUE
+from time import sleep
+from bs4 import BeautifulSoup as bs
+from numpy import double
+import requests
+import re
+
+old_price = 0
+
+while TRUE:
+    page = requests.get("https://www.google.com/finance/quote/EUR-RUB")
+    soup = bs(page.content, features="html.parser")
+    price = soup.find(class_="YMlKec fxKbKc").string
+    shit = re.sub('[^a-zA-Z]+', "", price)
+
+    if (double(price) > old_price):
+        old_price = double(price)
+
+    print("\nCurrent tairov family crisis represented in ruble: ", price)
+    print("Highest price recorded by me: ", old_price)
+    sleep(60)
